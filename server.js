2d88ec038dd92a1ad0c55da8eda9f7e9d1ebf64a
tamtam <tamtam113.tg@gmail.com>
2026-03-06 14:36:45 -0500

    Initial backend commit

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import authRoutes from "./routes/authroutes.js";
 import userRoutes from "./routes/userroutes.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -14,10 +17,11 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
-// Connect to MongoDB
-mongoose
-  .connect("mongodb://127.0.0.1:27017/kinder")
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
