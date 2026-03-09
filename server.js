aa8b4871773e26bcaaf55d7fef50e8a2b48c508d
tamtam <tamtam113.tg@gmail.com>
2026-03-09 08:55:45 -0400

    try

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import AuthRoutes from "./routes/authroutes.js";
+import authroutes from "./routes/authroutes.js";
 
 dotenv.config();
 
