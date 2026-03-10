93df68cd6b3c4d497eaaa20bcc6673d4d38f40ed
tamtam <tamtam113.tg@gmail.com>
2026-03-10 12:35:47 -0400

    y

--- server.js
+++ server.js
@@ -31,9 +31,10 @@ app.use("/api/auth", authroutes);
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
 });
+
 
 // ERROR HANDLER
 app.use((err, req, res, next) => {
